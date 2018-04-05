6d082324593f2556ed5175664470b709a9c8716e
KKD-SR <37869472+KKD-SR@users.noreply.github.com>
2018-04-05 19:20:40 +0530

    Update FarmerLoginServiceApplication.java

--- src/main/java/com/farmer/farmerloginservice/FarmerLoginServiceApplication.java
+++ src/main/java/com/farmer/farmerloginservice/FarmerLoginServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import brave.sampler.Sampler;
 
 @EnableEurekaDiscovery
-@EnableConfigServer                               
+                              
 @EnableSwagger2
 @EnableCircuitBreaker
 @EnableHystrixDashboard
